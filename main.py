3a871a50c0e47d729819932c8377f9bcd30a871d
varalysse <kaydanmann@gmail.com>
2025-10-10 17:27:34 -0700

    fixed verbose functionality

--- main.py
+++ main.py
@@ -7,29 +7,29 @@ from google.genai import types
 def main():
     load_dotenv()
     
+    #definitions
     args = sys.argv[1:]
-    verbose = '--verbose'
+    verbose_flag = '--verbose' in args
+
     #parse args
     if not args:
         print("AI Code Assistant")
         print('\nUsage: python main.py "your prompt here"')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
-    #join args
-    user_prompt = " ".join(args)
+
+    #user prompt
+    cleaned_args = [arg for arg in args if arg != '--verbose']
+    user_prompt = " ".join(cleaned_args)
+
     #build messages
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
-    #verbose
-    if verbose in args:
-        #print("User prompt: {user_prompt}")
-        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-        print("Response tokens:", response.usage_metadata.candidates_token_count)
+
     #call api to get response
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
@@ -37,6 +37,13 @@ def main():
     print("Response:")
     print(response.text)
 
+    #verbose addons
+    if verbose_flag:
+        pt = getattr(response.usage_metadata, "prompt_token_count", None)
+        rt = getattr(response.usage_metadata, "candidates_token_count", None)
+        print("User prompt:", user_prompt)
+        print("Prompt tokens:", pt)
+        print("Response tokens:", rt)
 
 if __name__ == "__main__":
     main()
