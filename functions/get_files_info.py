ce1b940a3ff7b19bc429cf0cc1e6378fa206d596
Persimmonly <lee@Mugi.localdomain>
2025-10-14 21:42:10 -0700

    checking directories

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,2 +1,5 @@
 def get_files_info(working_directory, directory="."):
+    relative_path = os.path.join(working_directory, directory)
+    if not os.path.abspath(relative_path, working_directory):
+        return f'Error: Cannot list {directory}" as it is outside the permitted working directory'
 
