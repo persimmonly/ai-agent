48d1dbe5bc8042850ccb0497a92d0b7580eb4eef
varalysse <kaydanmann@gmail.com>
2025-10-20 16:28:08 -0700

    get file content

--- calculator/tests.py
+++ calculator/tests.py
@@ -1,49 +1,16 @@
-# tests.py
-
-import unittest
-from pkg.calculator import Calculator
+from functions.get_file_content import get_file_content
 
 
-class TestCalculator(unittest.TestCase):
-    def setUp(self):
-        self.calculator = Calculator()
+def test():
+    result = get_file_content("calculator", "main.py")
+    print(result)
 
-    def test_addition(self):
-        result = self.calculator.evaluate("3 + 5")
-        self.assertEqual(result, 8)
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print(result)
 
-    def test_subtraction(self):
-        result = self.calculator.evaluate("10 - 4")
-        self.assertEqual(result, 6)
-
-    def test_multiplication(self):
-        result = self.calculator.evaluate("3 * 4")
-        self.assertEqual(result, 12)
-
-    def test_division(self):
-        result = self.calculator.evaluate("10 / 2")
-        self.assertEqual(result, 5)
-
-    def test_nested_expression(self):
-        result = self.calculator.evaluate("3 * 4 + 5")
-        self.assertEqual(result, 17)
-
-    def test_complex_expression(self):
-        result = self.calculator.evaluate("2 * 3 - 8 / 2 + 5")
-        self.assertEqual(result, 7)
-
-    def test_empty_expression(self):
-        result = self.calculator.evaluate("")
-        self.assertIsNone(result)
-
-    def test_invalid_operator(self):
-        with self.assertRaises(ValueError):
-            self.calculator.evaluate("$ 3 5")
-
-    def test_not_enough_operands(self):
-        with self.assertRaises(ValueError):
-            self.calculator.evaluate("+ 3")
+    result = get_file_content("calculator", "/bin/cat")
+    print(result)
 
 
 if __name__ == "__main__":
-    unittest.main()
+    test()
